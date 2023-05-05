364fe5480cd445ae9ce7da69cc7dc7eea59723eb
Khanh Tran <katetrannbkhanh@gmail.com>
2023-05-05 14:19:50 -0700

    commit tree 4a57b6fa793720cf3c625c4d98af3f69c2080f05

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
+require 'pry'
 class Person
+    def name
+        @name
+    end
 
+    def name=(name)
+        @name = name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(job)
+        @job = job
+    end
 end
+binding.pry
